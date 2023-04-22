f01b87c1b721835d9d9381e7623cbb2412b3021c
Lazlo22 <v.prihodski@gmail.com>
2023-04-22 15:12:11 +0300

    Implemented canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // components
 import Home from './pages/Home';
+import Canvas from './canvas';
 
 function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
+      <Canvas />
     </main>
   );
 }
