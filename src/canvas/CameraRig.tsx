9c3421ef00b866d04272a99a9f9934267be439b2
Lazlo22 <v.prihodski@gmail.com>
2023-07-10 13:39:07 +0300

    setup a hosting

--- src/canvas/CameraRig.tsx
+++ src/canvas/CameraRig.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { ReactNode, useRef } from 'react';
 import { useFrame, GroupProps } from '@react-three/fiber';
 import { easing } from 'maath';
@@ -20,6 +21,7 @@ const CameraRig = ({ children }: CameraRigProps) => {
 
     // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
+
     if(snap.intro) {
       if(isBreakpoint) targetPosition = [0, 0, 2];
       if(isMobile) targetPosition = [0, 0.2, 2.5];
